dd7200848e7f4e5db24c6aa6dc86e33803aa037b
sc30gsw <haidoushi875@gmail.com>
2022-05-30 06:53:51 +0900

    ログイン認証済みかどうかで描画するコンポーネントを変更する

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { SignIn } from "./components/SignIn";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import { Line } from "./components/Line";
 
+/**
+ * アプリケーションを構成するコンポーネント
+ *
+ * @returns
+ */
 export const App = () => {
-	return <div></div>;
+	// ログインユーザーの情報を取得
+	const user = useAuthState(auth);
+	// ユーザーが認証済みの場合、Lineコンポーネントを描画、認証済みでない場合は、SignInコンポーネントを描画
+	return <div>{user ? <Line /> : <SignIn />}</div>;
 };
