75fcf1078a0e8ea1bceb9b8e7615a2a9ec6f848b
sc30gsw <haidoushi875@gmail.com>
2022-05-30 07:05:11 +0900

    firebaseDBの設定

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
 
 // firebaseからの認証情報を取得
 export const auth = getAuth();
