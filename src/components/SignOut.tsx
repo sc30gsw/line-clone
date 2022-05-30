03496a1ad07990ad274011bbf5890273ebae3736
sc30gsw <haidoushi875@gmail.com>
2022-05-30 09:10:15 +0900

    サインアウトコンポーネントにスタイルを適用し、要素を追加する

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@mui/material";
 import { FC } from "react";
 import { auth } from "../firebase";
+import CallIcon from "@mui/icons-material/Call";
 
 /**
- * サインアウトに関するコンポーネント
+ * サインアウトボタンのコンポーネント
  *
  * @returns
  */
 export const SignOut: FC = () => {
 	return (
-		<div>
+		<div className="header">
 			{/* ボタン押下時サインアウトする */}
-			<Button onClick={() => auth.signOut()}>サインアウト</Button>
+			<Button
+				onClick={() => auth.signOut()}
+				style={{ color: "white", fontSize: "15px" }}
+			>
+				サインアウト
+			</Button>
+			{/* ログインユーザーのユーザー名を表示 */}
+			<h3>{auth.currentUser?.displayName}</h3>
+			<CallIcon />
 		</div>
 	);
 };
